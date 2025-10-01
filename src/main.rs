4d2227615df37248f59e4986e4dc05f99de3aa75
Jack Toumey <jack@toumey.io>
2025-09-30 19:28:37 -0700

    with match and one liner fns

--- src/main.rs
+++ src/main.rs
@@ -15,19 +15,23 @@ enum Media {
 
 impl Media {
   fn description(&self) -> String {
-    // return String::from(self.description())
-    // return String::from("Media Description")
-    if let Media::Book { title, author } = self {
-      format!("Book: {} {}", title, author)
-    }
-    else if let Media::Movie { title, director } = self {
-      format!("Movie: {} {}", title, director)
-    }
-    else if let Media::Audiobook { title } = self {
-      format!("Audiobook: {}", title)
-    }
-    else {
-      String::from("Media description")
+    // if let Media::Book { title, author } = self {
+    //   format!("Book: {} {}", title, author)
+    // }
+    // else if let Media::Movie { title, director } = self {
+    //   format!("Movie: {} {}", title, director)
+    // }
+    // else if let Media::Audiobook { title } = self {
+    //   format!("Audiobook: {}", title)
+    // }
+    // else {
+    //   String::from("Media description")
+    // }
+
+    match self {
+      Media::Book { title, author } => format!("Book: {} {}", title, author),
+      Media::Movie { title, director } => format!("Movie: {} {}", title, director),
+      Media::Audiobook { title } => format!("Audiobook: {}", title),
     }
   }
 }
